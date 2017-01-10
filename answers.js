e24937c88208f09d6ac16c7798f7c2394909aa37
Bryan LeBlanc <mrbeardbaron@gmail.com>
2017-01-10 19:17:31 +0000

    question 2

--- answers.js
+++ answers.js
@@ -7,3 +7,11 @@ var firstChar = function(word) {
 console.log(firstChar("Hello"));
 console.log(firstChar("anything"));
 console.log(firstChar(""));
+//2. Write a function that takes a string and returns the last character of a string.//
+//Test your function on a few inputs, including the empty string.//
+var lastChar = function(otherWord) {
+    return otherWord[(otherWord.length - 1)];
+};
+console.log(lastChar("GoodBye"));
+console.log(lastChar("nothing"));
+console.log(lastChar(""));
