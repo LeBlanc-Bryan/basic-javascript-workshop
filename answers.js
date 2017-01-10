1b95b441a3e76ed06bc2d92d07d923016979f456
Bryan LeBlanc <mrbeardbaron@gmail.com>
2017-01-10 19:28:14 +0000

    question 3

--- answers.js
+++ answers.js
@@ -15,3 +15,10 @@ var lastChar = function(otherWord) {
 console.log(lastChar("GoodBye"));
 console.log(lastChar("nothing"));
 console.log(lastChar(""));
+//3. Write a function that takes a string and a number, and returns the character at the position represented by the number.//
+//The indexing of number should start at 0. Test your function on a few inputs, including the empty string//
+var wordNumber = function(a,b) {
+    return a[(b-1)];
+    //a = string, b = number//
+};
+console.log (wordNumber("Mark", 2));
